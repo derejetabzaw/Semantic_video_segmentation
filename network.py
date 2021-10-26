15fee784a2e791017ae06d59f6078e11c98fec2b
Dereje <derejetabzaw@gmail.com>
2021-10-26 23:25:13 +0300

    added cnn and lstm models

--- network.py
+++ network.py
@@ -4,44 +4,44 @@ from keras.activations import sigmoid
 from keras.layers import CuDNNLSTM, Dropout
 from tensorflow.keras.layers import Flatten
 
+
 def cnn_for_mfcc(input):
     model = models.Sequential()
-    model.add(layers.Conv2D(13, 32, activation = 'relu', input_shape=input.shape))
-    model.add(layers.MaxPooling2D(6))
+    model.add(layers.Conv1D(32, 1, activation='relu', batch_input_shape=input.shape))
+    model.add(layers.MaxPooling1D((6)))
+    model.add(layers.Conv1D(64, (1), activation='relu'))
+    model.add(layers.MaxPooling1D((7)))
+    model.add(layers.Conv1D(128, (1), activation='relu'))
+    model.add(layers.MaxPooling1D((8)))
+    model.add(layers.Flatten())
+    model.summary()
+    return model(input)
 
-    model.add(layers.Conv2D(32, 64,activation = 'relu'))
-    model.add(layers.MaxPooling2D(7))
+def cnn_for_convfeats(input):
+    model = models.Sequential()
+    model.add(layers.Conv1D(1024, 1, activation = 'relu', input_shape=input.shape[1:]))
+    model.add(layers.MaxPooling1D(2))
 
-    model.add(layers.Conv2D(64, 128 , activation = 'relu'))
-    model.add(layers.MaxPooling2D(8))
+    model.add(layers.Conv1D(256, 1,activation = 'relu'))
+    model.add(layers.MaxPooling1D(2))
+
     model.add(Flatten())
     model.summary()
     return model(input)
 
 
-def cnn_for_convfeats(input):
-    model = models.Sequential()
-    model.add(layers.Conv1D(4096, 1024, activation = 'relu', input_shape=input.shape))
-    model.add(layers.MaxPooling1D(2))
-
-    model.add(layers.Conv1D(1024, 256,activation = 'relu'))
-    model.add(layers.MaxPooling1D(2))
-
-    model.add(Flatten())
-    return model(input)
-
-
 def cnn_for_csift(input):
     model = models.Sequential()
-    model.add(layers.Conv1D(384, 300, activation = 'relu', input_shape=input.shape))
+    model.add(layers.Conv1D(300, 1, activation='relu', batch_input_shape=input.shape))
     model.add(layers.MaxPooling1D(4))
 
-    model.add(layers.Conv1D(300, 256,activation = 'relu'))
+    model.add(layers.Conv1D(256, 1,activation = 'relu'))
     model.add(layers.MaxPooling1D(4))
 
-    model.add(layers.Conv1D(256, 128 , activation = 'relu'))
+    model.add(layers.Conv1D(128, 1 , activation = 'relu'))
     model.add(layers.MaxPooling1D(5))
     model.add(Flatten())
+    model.summary()
     return model(input)
 
 
--- train.py
+++ train.py
@@ -12,7 +12,6 @@ keyframes_directory = (dataset_directory + '/' + 'keyframes')
 
 color_SIFT_features = []
 MFCC_features = []
-dummy_tensors = tf.zeros([1,4096],tf.float32)
 count = 0
 
 
@@ -22,30 +21,28 @@ for root, dirs,files in os.walk(dataset_directory):
         if filename.endswith((".mp4",".flv",".mpg",".avi",".wmv",".mpv")):
             target_video = str(dataset_directory + '/' + filename)
             print ("Selecting Keyframes")
-            # keyframe(target_video)
+            keyframe(target_video)
             print ("Done Selecting")
             print ("Cleaning Up ...")
-            # clean_up(keyframes_directory)
+            clean_up(keyframes_directory)
             print ("Extracting Features")
             for key_root, key_dirs,keyframes in os.walk(keyframes_directory):
                 for images in keyframes:
                     if images.endswith((".jpeg",".jpg",".png")):
                         
-                        # keyframe_image = (keyframes_directory + '/' + images).replace("\\","/")
-                        # convolution_features = convfeats(keyframe_image)
-                        # if count == 0:
-                        #     convolution_features_tensors = convolution_features
-                        # else:
-                        #     convolution_features_tensors = Concatenate(axis = 0)([convolution_features_tensors,convolution_features])
-                        # print (convolution_features_tensors.shape)
-                        # cnn_outout_convfeats = cnn_for_convfeats(convolution_features_tensors)
-                        # print (cnn_outout_convfeats)
-                        # color_SIFT_features.append(CSIFT(keyframe_image,100)[1])
+                        keyframe_image = (keyframes_directory + '/' + images).replace("\\","/")
+                        convolution_features = convfeats(keyframe_image)
+                        if count == 0:
+                            convolution_features_tensors = convolution_features
+                        else:
+                            convolution_features_tensors = Concatenate(axis = 0)([convolution_features_tensors,convolution_features])
+                        color_SIFT_features.append(CSIFT(keyframe_image,100)[1])
                         count += 1
                 for audios in keyframes:
                     if audios.endswith((".wav")):
                         keyframe_audio = str(keyframes_directory + '/' + audios).replace("\\","/")
-                        MFCC_features.append(MFCC(keyframe_audio))
+                        keyframe_mfcc = MFCC(keyframe_audio)
+                        MFCC_features.append(keyframe_mfcc)
             print ("Done Extracting")
             
 
@@ -53,9 +50,22 @@ for root, dirs,files in os.walk(dataset_directory):
 
 color_SIFT_features = np.array(color_SIFT_features)
 MFCC_features = np.array(MFCC_features)
+convolution_features_tensors = np.array(convolution_features_tensors)
+convolution_features_tensors = convolution_features_tensors.reshape(1,convolution_features_tensors.shape[0],convolution_features_tensors.shape[1])
 print ("ConFeats to CNN")
 
-print (MFCC_features.shape)
+cnn_output_mfcc = np.array(cnn_for_mfcc(MFCC_features))
+cnn_output_csift = np.array(cnn_for_csift(color_SIFT_features))
+cnn_output_convfeats = np.array(cnn_for_convfeats(convolution_features_tensors))
 
-print (cnn_for_mfcc(MFCC_features))
 
+
+input_for_lstm = np.concatenate((cnn_output_mfcc.flatten(),cnn_output_csift.flatten(),cnn_output_convfeats.flatten()),axis = None)
+input_for_lstm = input_for_lstm.reshape(1,1,input_for_lstm.shape[0])
+# input_for_lstm = np.array((cnn_output_mfcc.flatten(),cnn_output_csift.flatten(),cnn_output_convfeats.flatten()))
+
+lstm = tf.keras.layers.LSTM(500)
+output = lstm(input_for_lstm)
+
+print (output)
+print (output.shape)
